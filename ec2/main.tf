1f414f7dbbc73441db4a377aadc1d8a269ed145c
akash911133 <akashughade77@gmail.com>
2024-08-01 01:27:26 +0530

    NCP-1011:  Make it more redable terraform code

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,8 +1,13 @@
+
+#############  1st we define provider 
+
 provider "aws" {
   region = "ap-south-1"
   profile = "akash-ughade"
 }
 
+#############  2nd we define what type of required provide we want 
+#############  and version 
 
 terraform {
   required_providers {
@@ -15,10 +20,12 @@ terraform {
   required_version = ">= 1.5.6"
 }
 
+#############  ec2  creation   
+
 resource "aws_instance" "My-Linux-1" {
   ami           = "ami-068e0f1a600cd311c"
   instance_type = "t2.micro"
-
+  security_groups = [var.my_sg]
   tags = {
     Name           = "Akash Ughade"
     Designation    = "DevOps Engineer"
@@ -26,4 +33,23 @@ resource "aws_instance" "My-Linux-1" {
     Specification  = "Hands-on Terraform & Ansible"
     Organization   = "NCS Indore"
   }
+}
+
+#############  variables we want
+
+variable "my_sg" {
+  default = "launch-wizard-1"
+  type    =  string
+}
+
+
+
+#############  Ouputs we want 
+
+output "public_ip" {
+  value = aws_instance.My-Linux-1.public_ip
+}
+
+output "private_ip" {
+  value = aws_instance.My-Linux-1.private_ip
 }
