d4654628ff36439ef45a62900ff14c2828919957
akash911133 <akashughade77@gmail.com>
2024-08-01 01:48:04 +0530

    NCP-1011:  Make it more redable terraform code

--- ec2/main.tf
+++ ec2/main.tf
@@ -25,13 +25,41 @@ terraform {
 resource "aws_instance" "My-Linux-1" {
   ami           = "ami-068e0f1a600cd311c"
   instance_type = "t2.micro"
-  security_groups = [var.my_sg]
+  security_groups = [var.my_custom_sg1]
   tags = {
     Name           = "Akash Ughade"
     Designation    = "DevOps Engineer"
     OS             = "Amazon Linux RHEL"
     Specification  = "Hands-on Terraform & Ansible"
     Organization   = "NCS Indore"
+  }
+}
+
+
+resource "aws_security_group" "my_custom_sg1" {
+  name        = "my-custom security group"
+  description = "Security group application HOSTING secure and unsecure port open"
+
+  // Inbound rules
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  // Allow SSH from anywhere
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  // Allow HTTP from anywhere
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  // Allow HTTPS from anywhere
   }
 }
 
--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,6 +1,6 @@
 #############  variables we want
 
-variable "my_sg" {
+variable "my_custom_sg1" {
   default = "launch-wizard-1"
   type    =  string
 }
